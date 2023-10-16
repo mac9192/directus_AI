ba7736f29848bb2ca49f56f9de151cf329c0b9c8
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-10-16 14:18:00 -0400

    Fixed rateLimiter import

--- chatbot-middleware.ts
+++ chatbot-middleware.ts
@@ -1,7 +1,7 @@
 // middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { rateLimiter } from '../fullstack_blog/chatbot-lib/rate-limiter'
+import { rateLimiter } from './chatbot-lib/rate-limiter'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
